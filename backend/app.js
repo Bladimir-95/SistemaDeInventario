df86a8d408040ebb05528cde1ba8037b910555d3
Oscar <oscar95rauda@gmail.com>
2026-08-09 23:08:40 -0600

    Conexion y primera consulta a la base de datos

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const cors = require('cors')
+
 const app = express()
 
 const productRoutes = require('./src/routes/products.routes')
 
+app.use(cors())
 app.use(express.json())
 
 app.use('/api/products', productRoutes)
--- frontend/src/pages/procuts/getProducts/GetProducts.tsx
+++ frontend/src/pages/procuts/getProducts/GetProducts.tsx
@@ -1,42 +1,38 @@
 import style from "./GetProducts.module.css";
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 type Product = {
   id: number;
   name: string;
+  description: string;
   price: number;
+  stock: number;
   image: string;
-  stock?: number;
-};
+  category_id: number;
+  status: number;
+  created_at: string;
+  updated_at: string;
+}
 
 function GetProducts() {
-  const [products, setProducts] = useState<Product[]>([
-    {
-      id: 1,
-      name: "laptop Gamer",
-      price: 1200,
-      stock: 3,
-      image:
-        "https://i5.walmartimages.com/seo/ASUS-ROG-Strix-SCAR-15-G533QS-DS76-AMD-Ryzen-7-5800H-4-4-GHz-Windows-10-Home-GF-RTX-3080-16-GB-RAM-1-TB-SSD-NVMe-15-6-1920-x-1080-Full-HD-300-Hz-Giga_dcab559f-0914-43e1-bda7-cc99f820da28.b6ef2d60d9b688ffede8c036c31956d8.jpeg",
-    },
-    {
-      id: 2,
-      name: "Teclado Mecánico",
-      price: 150,
-      image:
-        "https://cyberteamcr.com/wp-content/uploads/2024/11/3754610-cb069311.webp",
-      stock: 2,
-    },
-    {
-      id: 3,
-      name: "Mouse Gamer",
-      price: 80,
-      image:
-        "https://www.steren.com.sv/media/catalog/product/cache/bb0cad18a6adb5d17b0efd58f4201a2f/image/2302839e9/mouse-usb-gamer-800-1600-2400-3200-4800-7200-dpi.jpg",
-      stock: 9,
-    },
-  ]);
+  const [products, setProducts] = useState<Product[]>([])
+
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/api/products");
+        const data: Product[] = await response.json();
+        console.log(data)
+
+        setProducts(data);
+      } catch (error) {
+        console.error(error)
+      }
+    };
+
+    getProducts();
+  }, []);
 
   const handleDelete = (id: number) => {
     const confirmDelete = confirm("¿Seguro que quieres eliminar este producto?");

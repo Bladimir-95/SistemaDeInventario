0f25a15b71b5633a9cde797dea25b5d5039ecb09
Oscar <oscar95rauda@gmail.com>
2026-08-19 19:50:41 -0600

    Logica reparada en el componente EditProduct

--- frontend/src/pages/procuts/editProduct/EditProduct.tsx
+++ frontend/src/pages/procuts/editProduct/EditProduct.tsx
@@ -1,47 +1,50 @@
 import { useParams } from "react-router-dom";
 import CreatProducts from "../createProducts/CreatProducts";
+import { useEffect, useState } from "react";
 
 
 type Product = {
   id: number;
   name: string;
-  category: string;
+  category_id: number;
   price: number;
   stock: number;
   image?: string;
 };
 
-// TEMPORAL (luego vendrá de estado o backend)
-const products: Product[] = [
-  {
-    id: 1,
-    name: "laptop Gamer",
-    price: 1200,
-    stock: 3,
-    category: "tecnologia",
-  },
-  {
-    id: 2,
-    name: "Teclado Mecánico",
-    price: 150,
-    stock: 2,
-    category: "tecnologia",
-  },
-  {
-      id: 3,
-      name: "Mouse Gamer",
-      price: 80,
-      stock: 9,
-      category: "Tecnologia"
-    },
-];
+type Props = {
+  product?: Product
+}
 
 function EditProduct() {
   const { id } = useParams();
 
-  const product = products.find(p => p.id === Number(id));
+  const [product, setProduct] = useState<Product | null>(null);
 
-  if(!product) return <p>Producto no encontrado</p>
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const response = await fetch(`http://localhost:3000/api/products/${id}`);
+
+        if(!response.ok) {
+          throw new Error("Producto no encontrado");
+        }
+
+        const data = await response.json();
+
+        setProduct(data);
+
+      } catch (error) {
+        console.log("Error al encontrar producto", error);
+      }
+    }
+
+    getProducts();
+  },[id])
+
+  if (!product) {
+    return <p>Producto no encontrado</p>;
+  }
 
   return <CreatProducts product={product}/>;
 }
